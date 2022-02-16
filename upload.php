24f444b8401ffe21019b0709d63982eb145d620a
ClassicDazzik <58544514+ClassicDazzik@users.noreply.github.com>
2022-02-16 08:39:09 +0200

    Updated files

--- upload.php
+++ upload.php
@@ -1,5 +1,4 @@
 <?php
-$htmlpage = fopen("uploader.html", "r");
 
 //$imgfile = $_POST['fileToUpload'];
 $sendername = $_POST['sender'];
@@ -17,5 +16,28 @@ if(isset($_POST["submit"])) {
         echo "This is not an image!";
         $uploadOk = 0;
     }
+  }
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+  }
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+  }
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+  && $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+  }
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+  // if everything is ok, try to upload file
+  } else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+      echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+    } else {
+      echo "Sorry, there was an error uploading your file.";
+    }  
 }
 ?>
