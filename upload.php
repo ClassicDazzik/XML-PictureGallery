ac4d16da29b65a7e83129770174cf85939ad19d3
ClassicDazzik <58544514+ClassicDazzik@users.noreply.github.com>
2022-02-21 23:32:59 +0200

    Started a bit of work on the xml part

--- upload.php
+++ upload.php
@@ -1,8 +1,8 @@
 <?php
 
-//$imgfile = $_POST['fileToUpload'];
-$sendername = $_POST['sender'];
-
+$xml = simplexml_load_file('gallery/data.xml');
+$senderName = $_POST['sender'];
+$imgDate = date('d-n-y')
 $imgFolder = "img/";
 $imgFile = $imgFolder . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
@@ -17,7 +17,7 @@ if(isset($_POST["submit"])) {
         $uploadOk = 0;
     }
   }
-if (file_exists($target_file)) {
+if (file_exists($imgFile)) {
     echo "Sorry, file already exists.";
     $uploadOk = 0;
   }
@@ -25,19 +25,26 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
   }
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-  && $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+if($imgFileType != "jpg" && $imgFileType != "png" && $imgFileType != "jpeg") {
+    echo "Sorry, only JPG and PNG files are allowed.";
     $uploadOk = 0;
   }
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
-  // if everything is ok, try to upload file
   } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $imgFile)) {
       echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
     } else {
       echo "Sorry, there was an error uploading your file.";
     }  
+}
+
+$xmlAdd = $xml->addChild('img');
+
+if ($uploadOk == 1) {
+  $xmlAdd->addChild('name', $imgFile);
+  $xmlAdd->addChild('sendername', $senderName);
+  $xmlAdd->addChild('date', $imgDate)
+  $xmlAdd->addAttribute('accepted', '0')
 }
 ?>
---
+++ xmlcheck.php
@@ -0,0 +1,4 @@
+<?php
+$xml = simplexml_load_file('gallery/data.xml');
+var_dump($xml);
+?>
