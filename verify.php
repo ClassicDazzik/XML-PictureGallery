a1d54aa1df3ef1d40bdad30d4be3a1ad8ec4cad5
ClassicDazzik <58544514+ClassicDazzik@users.noreply.github.com>
2022-03-14 11:50:48 +0200

    Juttuja

--- verify.php
+++ verify.php
@@ -7,20 +7,25 @@
 <body>
 
 <?php
-if (isset($_GET['delete']) && isset($_GET['id'])){
-    echo `<p>Poistetaan kuva {$_GET['id']}</p>`;
-} 
-?>
-
-
-<?php
 $xml = simplexml_load_file('data.xml');
 $i = 0;
+
+if (isset($_GET['delete']) && isset($_GET['id'])){
+    echo `<p>Poistetaan kuva {$_GET['id']}</p>`;
+    unlink();
+    unset($xml->img[$i]);
+}
+
+$dom = new DOMDocument("1.0");
+$dom->preserveWhiteSpace = false;
+$dom->formatOutput = true;
+$dom->loadXML($xml->asXML());
+$dom->save('data.xml');
 ?>
 
 <?php foreach ($xml->img as $img): ?>
    
-    <img src="img/<?php echo $img->name ?>" alt="<?php echo $img->sendername; ?>" style="width:600px;height:400px"><br>
+    <img src="<?php echo $img->name ?>" alt="<?php echo $img->sendername; ?>" style="width:600px;height:400px"><br>
    
     <?php if ($img['accepted'] == '0'): ?>
         <form action="" method="">
